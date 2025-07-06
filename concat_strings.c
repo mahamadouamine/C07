95c4b4e63f907fac2544bad4433104f6eaf88c16
Amine <mamoudousouleymahamadouaminou@gmail.com>
2025-07-06 15:17:49 +0100

    commit 4

--- concat_strings.c
+++ concat_strings.c
@@ -5,7 +5,7 @@ int main() {
     char chaine1[50], chaine2[50], resultat[100];
     scanf("%s", chaine1);
     scanf("%s", chaine2);
-    sprintf(resultat,"%s%s", chaine1, chaine2);
-    printf("%s", resultat);
+    sprintf(resultat, "%s%s", chaine1, chaine2);
+    printf(" %s\n", resultat);
     return 0;
 }
