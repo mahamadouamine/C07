dad272cecae68c44cb095c27e91bf1726d91ee45
Amine <mamoudousouleymahamadouaminou@gmail.com>
2025-07-10 07:19:29 +0100

    commit 7

---
+++ is_palindrome.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+  char chaine[50]; // Exemple de chaîne
+  int debut = 0;
+  int fin = 0;
+  int Palindrome = 1; // Assume que c'est un palindrome au départ
+  scanf("%s", chaine);
+  // Calculer la longueur de la chaîne sans utiliser strlen
+  while (chaine[fin] != '\0') {
+    fin++;
+  }
+  fin--; // Ajuster pour que 'fin' pointe sur le dernier caractère
+
+  // Comparer les caractères en partant des extrémités
+  while (debut < fin) {
+    if (chaine[debut] != chaine[fin]) {
+      Palindrome = 0; // Ce n'est pas un palindrome
+      break; // Sortir de la boucle dès qu'une différence est trouvée
+    }
+    debut++;
+    fin--;
+  }
+
+  // Afficher le résultat
+  if (Palindrome) {
+    printf("\"%s\" est un palindrome.\n", chaine);
+  } else {
+    printf("\"%s\" n'est pas un palindrome.\n", chaine);
+  }
+
+  return 0;
+}
